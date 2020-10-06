5143756d9cc1810205bccd6cf2182cf50bc5b2bb
YOHAN <widogue.yohan@gmail.com>
2020-10-06 16:57:14 +0200

    Clear code

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 
-
-
-a=2;
 async function load(){
     try{
         
         const response = await fetch('https://jsonplaceholder.typicode.com/todos')
-        const task = await response.json();
-        
-        task.forEach(element =>
-           newLign(element.id,element.title,element.completed)
+        const tasks = await response.json();
+
+        tasks.forEach(element =>
+           newLign(element.id,element.title,element.completed),
+           
         );
+
+        showHit(tasks.length);
+
     }
     catch(e) {
         console.error(e);
@@ -19,18 +20,20 @@ async function load(){
 }
 
 function newLign(id,title,completed){
-    console.log(id,title,completed)
     const body=document.getElementById('table-body');
     const lign=document.createElement('tr');
-    
     body.appendChild(lign);
     lign.innerHTML+=`<td>${id}</td>`;
     lign.innerHTML+=`<td>${title}</td>`;
-    
-        
     lign.innerHTML+=`<td id="${completed}">${completed}</td>`;
+}
 
-    
-    
+function showHit(hit){
+    document.getElementById('hit').innerHTML+=`<p>Le nombre de reponse est de : ${hit}`
+}
+
+
+function send(){
+    nameTask = document.getElementById('title').value
     
 }
