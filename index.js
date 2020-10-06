5ce0018f5c0d563f8f696402dce73b1757504b72
YOHAN <widogue.yohan@gmail.com>
2020-10-06 08:20:05 +0200

    new lign

--- index.js
+++ index.js
@@ -5,9 +5,22 @@
 async function load(){
     try{
          await fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(response => response.json()).then(console.log)
+    .then(response => response.json()).then(console.log);
     }
     catch(e) {
         console.error(e);
     }
+    response.forEach(element => {
+        
+    });
+}
+
+function newLign(){
+    const body=document.getElementById('table-body');
+    const lign=document.createElement('tr');
+    const element=document.createElement('td');
+
+    body.appendChild(lign);
+    lign.appendChild(element);
+
 }
