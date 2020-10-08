7b6328c6b8b1ebeff70406d29c4ac9704ea3b4d6
YOHAN <widogue.yohan@gmail.com>
2020-10-08 07:33:20 +0200

    Creae task done

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ async function load(){
     try{
         
         const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+        
+        
+
         const tasks = await response.json();
 
         tasks.forEach(element =>
@@ -35,5 +38,29 @@ function showHit(hit){
 
 function send(){
     nameTask = document.getElementById('title').value
+    choiceCompleted = document.getElementById('choice').value
+    var request = fetch('https://jsonplaceholder.typicode.com/todos', { 
+      
+        // Adding method type 
+        method: "POST", 
+          
+        // Adding body or contents to send 
+        body: JSON.stringify({ 
+            title: nameTask,
+            completed: choiceCompleted
+        }), 
+          
+        // Adding headers to the request 
+        headers: { 
+            "Content-type": "application/json; charset=UTF-8"
+        } 
+    }) 
+      
+    // Converting to JSON 
+    .then(response => response.json()) 
+    
+    // Displaying results to console 
+    .then(json => newLign(json.id,json.title,json.completed)); 
     
 }
+
